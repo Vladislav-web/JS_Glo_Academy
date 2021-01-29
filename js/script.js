544940c2de0353a1cb8cda4c0ab2ff0d1cf15d7d
Vladislav-web <ykt.pc_master@mail.ru>
2021-01-29 08:57:22 +0300

    added lesson02

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-const money = '30',
-      income = '70',
-      addExpenses = true,
-      deposit = '10',
-      mission = 'completed',
-      period = '3';
+const money = 100000,
+      income = 'фриланс',
+      addExpenses = "'Интернет', 'такси', 'коммуналка'",
+      deposit = true,
+      mission = 10000000,
+      period = 3;
+      budgetDay = 80000 / 30; 
 
-console.log('Хочу изучить JS!');
+      console.log(typeof money);
+      console.log(typeof income);
+      console.log(typeof deposit);
+      console.log(addExpenses.length);
+      console.log(`'Период равен ${period} месяцев' и 'Цeль заработать ${mission} рублей'`);
+      console.log(addExpenses.toLowerCase().split(' '));
+      console.log(budgetDay);
+
+     
